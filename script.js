f87ffcd4f964a1b48fafca96991abbb513be474a
Samiyah0923 <49485694+Samiyah0923@users.noreply.github.com>
2019-04-10 09:34:49 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+$(".temperature-button").click(function() {
+    var temperature = parseInt($(".temperature-input").val());
+    
+    if(temperature <= 50) {
+        $(".output").text("Wear a winter jacket");
+        
+        else if(temperature<= 80)
+        $().text()
+    }
+    // start your code here 
+   
+});
